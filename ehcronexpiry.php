e28684e28145960e8e7684e98bf1aad168612927
Eric Hall <ehdesign@me.com>
2018-04-16 16:18:20 -0400

    Main File
    Updated mail file outside of Git.  Pushing full file.

--- ehcronexpiry.php
+++ ehcronexpiry.php
@@ -10,27 +10,6 @@ License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-/*
-Check out page-aclsidcard.php (and the rest) in the root of Dynamic-Child - these were created by Element to dynamically create the digital cards and certificates. you’ll find references to completion and expiration.
-
-This is code from the page Barnes referenced:
-
-	global $current_user;
-	wp_get_current_user();
-
-	$course_id = 7422;
-
-	$user_cert_date = get_user_meta( $current_user->ID, 'NRP Cert Date', true );
-	$user_from_date = date('M j, Y', strtotime('+0 years', strtotime($user_cert_date)));
-	$user_new_date = date('M j, Y', strtotime('+2 years', strtotime($user_cert_date)));
-
-	$course_end = WooThemes_Sensei_Utils::sensei_check_for_activity( array( 'post_id' => intval( $course_id ), 'user_id' => intval( $current_user->ID ), 'type' => 'sensei_course_status' ), true );
-	$course_end_date = $course_end->comment_date;
-	$course_from_date = date('M j, Y', strtotime('+0 years', strtotime($course_end_date)));
-	$course_new_date = date('M j, Y', strtotime('+2 years', strtotime($course_end_date)));
-
-*/
-
 register_activation_hook( __FILE__, 'activateFunction' );
 register_deactivation_hook( __FILE__, 'deactivateFunction' );
 
@@ -42,42 +21,50 @@ function ehExpiry_setup_menu(){
 
 function ehCronExpiry_init(){
 	// this is called when the page loads
-	//TITLE
 	echo "<h1>Welcome to the Cron &amp; Find Expiration Page!</h1>";
 	//Time of Next Run
-	$timestamp = wp_next_scheduled('hourlyEvent3'); 
+	$timestamp = wp_next_scheduled('ehD_event'); 
 	$dt = new DateTime("@$timestamp");
 	$dt->setTimeZone(new DateTimeZone('America/New_York'));
 	echo '<p><strong>The next scheduled run is:</strong><br />';
 	echo $dt->format('F j, Y, g:ia');
 	echo ' (Eastern Time)';
-	echo '</p>';	
+	echo '</p>';
+	echo '<p><strong>The following will be updated at next run:</strong><br />';
+	global $wpdb;	
+	$results=$wpdb->get_results("SELECT comment_id, user_id, comment_date FROM wp_comments WHERE comment_post_ID = 8942 AND comment_approved LIKE 'complete' AND comment_type LIKE 'sensei_course_status' AND (YEAR(comment_date)=YEAR(CURRENT_DATE())-1) AND (DAYOFMONTH(comment_date)=DAYOFMONTH(CURRENT_DATE())) AND (MONTH(comment_date)=MONTH(CURRENT_DATE())+2) ORDER BY comment_date DESC");
+	$count = 0;
+	foreach($results as $result) {
+		$count++;
+		$user_info = get_userdata($result->user_id);		
+		$renewdate = date('Y-m-d', strtotime('+2 years', strtotime($result->comment_date)));
+		echo "#" . $count . "- " . $user_info->user_email . " passed on " . date('m/d/y',strtotime($result->comment_date)) . " at " . date('h:i a',strtotime($result->comment_date)) . "<br />";
+	}
+	echo "</p>";
 }
 
 function activateFunction() {
 	//this is called when the plugin is activated
 	//ADD the cron
-	wp_schedule_event(time(), 'hourly', 'hourlyEvent3');
+	wp_schedule_event(time(), 'hourly', 'ehD_event');
 }
 
 function deactivateFunction() {
 	//this is called when the plugin is deactivated
 	//REMOVE the cron
-	wp_clear_scheduled_hook('hourlyEvent3');
+	wp_clear_scheduled_hook('ehD_event');
 }
 
-function ehD_hourlyFunction() {
-	// Hourly Function via WP Cron
-	//Find all users with an expiration date 30 days in the future - erichall68->103800
-	/*
-	global $wpdb;
-	$results=$wpdb->get_results("SELECT userID, expireDate FROM testDate WHERE expireDate >= NOW() AND TO_DAYS(expireDate)-TO_DAYS(NOW())=30");
+function ehD_schedule_function() {
+	//Find All users with expire two months in past - 1 month window
+	global $wpdb;	
+	$results=$wpdb->get_results("SELECT comment_id, user_id, comment_date FROM wp_comments WHERE comment_post_ID = 8942 AND comment_approved LIKE 'complete' AND comment_type LIKE 'sensei_course_status' AND (comment_date <= DATE_SUB(NOW(),INTERVAL 12 MONTH)) AND (comment_date >= DATE_SUB(NOW(),INTERVAL 13 MONTH)) ORDER BY comment_date DESC");
 	
 	// Loop Through and Add them to Infusionsoft and add a tag to them
+	/*
 	foreach($results as $result) {
-		//THIS IS ALL TESTING... erichall68 = 103800
 		require_once('Infusionsoft/infusionsoft.php');
-		$user_info = get_userdata($result->userID);
+		$user_info = get_userdata($result->user_id);
 		$email = $user_info->user_email;
 		$groupID = 123;
 		//look up InfusionSoft ID from email
@@ -109,9 +96,7 @@ function ehAddSettingsLink( $links ) {
 }
 
 //Add Filters / Actions / etc.
-add_action('hourlyEvent3', 'ehD_hourlyFunction');
+add_action('ehD_event', 'ehD_schedule_function');
 $plugin = plugin_basename( __FILE__ );
 add_filter( "plugin_action_links_$plugin", 'ehAddSettingsLink' );
-
-
 ?>
